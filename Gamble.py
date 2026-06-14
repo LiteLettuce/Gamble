bed0e7babbcfe0175f7708cf79d45ec35fc1de90
Litelettuce <kevinjuniorthefourth@gmail.com>
2026-06-13 22:10:04 -0700

    Starting commit, simple project i made

--- Gamble.py
+++ Gamble.py
@@ -1,4 +1,4 @@
-import random, os
+import random, os, time
 
 direct = "GAMBLINGLOG"
 filed = "gamblinglog.txt"
@@ -14,9 +14,10 @@ class Account:
     def gamble(self, amount):
         self.money -= amount
         print(f"Gambled ${amount}")
+        global gambled, jackpot, lottery
         gambled = random.randint(1, 4)
-        global jackpot
         jackpot = random.randint(1, 15)
+        lottery = random.randint(1, 50)
         if gambled == 1:
             self.money += amount * 2
             print("You won your gamble.")
@@ -27,7 +28,10 @@ class Account:
             print("YOU JUST HIT THE JACKPOT!!!")
         else:
             print("You've lost the jackpot.")
-        return jackpot
+        if lottery == 1:
+            self.money += 999999
+            print("HOLY SMOKES, YOU HIT THE LOTTERY!!!!!")
+        return jackpot, gambled, lottery
     def check(self):
         print(f"Your money is: ${self.money}")
 
@@ -36,7 +40,10 @@ class Account:
             file.write(f"Money left over is ${self.money}\n")
             if jackpot == 1:
                 file.write("You also won the jackpot. (+9999)\n")
-
+            if gambled == 1:
+                file.write("You won the prize. (gambled amount * 2)\n")
+            if lottery == 1:
+                file.write("YOU HIT THE LOTTERY! (+999999)\n")
 shuffle = random.randint(1000, 5000)
 shuffle2 = random.randint(500, 1000)
 account = Account("Lettuce", shuffle)
@@ -45,3 +52,4 @@ account.check()
 account.gamble(shuffle2)
 account.check()
 account.write()
+time.sleep(60000)
