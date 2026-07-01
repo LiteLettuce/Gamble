f2d43cbc9120773a443e33c7684855d9d5d0cd06
Litelettuce <kevinjuniorthefourth@gmail.com>
2026-07-01 14:37:44 -0700

    Added logs button

--- Gamble.py
+++ Gamble.py
@@ -1,4 +1,4 @@
-import random, os, customtkinter, time
+import random, os, sys, customtkinter, datetime
 
 direct = "GAMBLINGLOG"
 filed = "gamblinglog.txt"
@@ -12,7 +12,6 @@ class Account:
         self.lost = False
         global factor
         factor = 0
-        self.loanmoney = False
         self.gambled = 0
         self.jackpot = 0
         self.lottery = 0
@@ -20,19 +19,16 @@ class Account:
         factor = random.randint(1, 6)
         if factor == 1:
             self.gambled = random.randint(1, 6)
-            self.jackpot = random.randint(1, 150)
-            self.lottery = random.randint(1, 1500)
-            print("y")
-        elif factor == 6:
-            self.gambled = random.randint(1, 3)
-            self.jackpot = random.randint(1, 70)
-            self.lottery = random.randint(1, 750)
-            print("n")
-        else:
-            self.gambled = random.randint(1, 4)
             self.jackpot = random.randint(1, 100)
             self.lottery = random.randint(1, 1000)
-            print("s")
+        elif factor == 6:
+            self.gambled = random.randint(1, 3)
+            self.jackpot = random.randint(1, 50)
+            self.lottery = random.randint(1, 500)
+        else:
+            self.gambled = random.randint(1, 4)
+            self.jackpot = random.randint(1, 70)
+            self.lottery = random.randint(1, 750)
 
     def gamble(self, amount):
         if self.lost == True:
@@ -60,15 +56,13 @@ class Account:
                 label_var.configure(text=f"Your in too much debt to gamble.\n Your money is {self.money}")
         else:
             self.money += amount
-            self.money -= 500
-            self.loanmoney = True
             self.takeout += 1
             label_var.configure(text=f"Your money is now ${self.money}")
         print(f"Got loan of {amount}")
 
     def write(self):
         with open(full_path, "a") as file:
-            file.write(f"Money left over is ${self.money}\n")
+            file.write(f"On {datetime.datetime.now()}:\nMoney left over is ${self.money}\n")
             if self.jackpot == 1:
                 file.write("You also won the jackpot. (+9999)\n")
             if self.gambled == 1:
@@ -77,14 +71,6 @@ class Account:
                 file.write("YOU HIT THE LOTTERY! (+999999)\n")
             if self.lost == True:
                 file.write("Your in debt, congrats\n")
-            if self.loanmoney == True:
-                file.write("Gotta pay interest somehow. (-5000)\n")
-            if factor == 1:
-                file.write("Values changed (lower chance of winning...)\n")
-            elif factor == 6:
-                file.write("Values changed (higher chance of winning!)\n")
-            else:
-                file.write("Values changed (normal chance of winning.)\n")
 class Rerun():
     def __init__(self):
         self.amount = random.randint(1, 1000)
@@ -113,6 +99,11 @@ class Rerun():
             label_var.configure(text="Values are higher than normal.")
         else:
             label_var.configure(text="Values are the normal.")
+    def log(self):
+        direc = os.path.dirname(os.path.abspath(sys.argv[0]))
+        path = os.path.join(direc, "GAMBLINGLOG")
+        path = os.path.realpath(path)
+        os.startfile(path)
 def app():
     global label_var, account, run
 
@@ -133,14 +124,16 @@ def app():
     label_var.pack(pady=20)
 
     gamble_button = customtkinter.CTkButton(theme, text="Gamble", fg_color="#094E18", corner_radius=100, command=run.run_gamble)
-    gamble_button.pack(pady=20)
+    gamble_button.pack(pady=15)
     gamble2_button = customtkinter.CTkButton(theme, text="GO BIG OR GO HOME! (+10)", fg_color="#0084FF", corner_radius=100, command=run.ruinyourlife)
-    gamble2_button.pack(pady=20)
+    gamble2_button.pack(pady=15)
     gamble3_button = customtkinter.CTkButton(theme, text="GAMBLING IS MY LIFE FORCE!!! (+50)", fg_color="#7F0D8A", corner_radius=100, command=run.loop)
-    gamble3_button.pack(pady=20)
+    gamble3_button.pack(pady=15)
     loan_button = customtkinter.CTkButton(theme, text="Click to get a loan", fg_color="#0BC20B", corner_radius=100, command=run.bypass)
-    loan_button.pack(pady=20)
+    loan_button.pack(pady=15)
     check_button = customtkinter.CTkButton(theme, text="Click to see values of winning", fg_color="#FF7300", corner_radius=100, command=run.values)
-    check_button.pack(pady=20)
+    check_button.pack(pady=15)
+    logs_button = customtkinter.CTkButton(theme, text="Click to open logs", fg_color="#19181D", corner_radius=100, command=run.log)
+    logs_button.pack(pady=15)
     root.mainloop()
 app()
